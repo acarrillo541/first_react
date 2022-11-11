690928605baa95eecdc3b8f522cb27d7743f5653
Alan Carrillo <acarrillo@Alans-MacBook-Pro.local>
2022-11-10 18:23:42 -0800

    tic tac toe tutorial

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const App = () => {
     return(
-        <h1> Hello how are you </h1>
+        <>
+            <h1> Hello how are you </h1>
+            <ul> 
+                <li> hello</li>
+                <li> how</li>
+                <li> are</li>
+            </ul>
+        </>
+
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
-import App from './App'
+//import App from './App'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+class Square extends React.Component{
+    render(){
+        return(
+            <button className="square">
+                {/*todo*/}
+            </button>
+        )
+    }
+}
+
+class Board extends React.Component{
+    renderSquare(i){
+        return <Square/>;
+    }
+
+    render() {
+        const status = 'Next player: X';
+    
+        return (
+          <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+            </div>
+          </div>
+        );
+      }
+}
+
+ReactDOM.render(<Board/>, document.getElementById('root'));
